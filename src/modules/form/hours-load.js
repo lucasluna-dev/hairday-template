c162e02ca7e5ac42ac627ffc8bd70d9091b07e9c
luna <lucassilvavg9@gmail.com>
2026-07-19 16:06:33 -0300

    adicionarndo funcao return apos a verificacao de horas

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,14 +1,25 @@
 import dayjs from "dayjs"
-import {openingHours} from "../../utils/opening-hours.js"
+import { openingHours } from "../../utils/opening-hours.js"
 
-export function hoursLoad({date}){
-    const opening = openingHours.map((hour)=>{
+export function hoursLoad({ date }) {
+    const opening = openingHours.map((hour) => {
 
         //recupera somento a hora
-       const [scheduleHour] = hour.split(":")
-       
-       // Adicionar a hora na data e verificar se está no passado.
-       const isHourPast = dayjs(date).add(Number(scheduleHour), "hour").isBefore(dayjs())
-       console.log(scheduleHour,isHourPast)
+        const [scheduleHour] = hour.split(":")
+
+        // Adicionar a hora na data e verificar se está no passado.
+        const isHourPast = dayjs(date).add(Number(scheduleHour), "hour").isAfter(dayjs())
+
+        return {
+            hour,
+            isHourPast: isHourPast
+        }
+
+        /* console.log({
+             hour,
+             isHourPast: isHourPast
+         })*/
+
+
     })
 }
