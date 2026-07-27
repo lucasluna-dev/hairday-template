b5a5457852710c540c0923cc0e6babcb40943a09
Lucas Luna <lunadev@DESKTOP-DER56QI.localdomain>
2026-07-27 19:59:00 -0300

    inclusao de horas validas no front-end

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours.js"
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({ date }) {
     const opening = openingHours.map((hour) => {
 
@@ -12,13 +14,26 @@ export function hoursLoad({ date }) {
 
         return {
             hour,
-            isHourPast: isHourPast
+            avaliable: isHourPast,
         }
 
         /* console.log({
              hour,
-             isHourPast: isHourPast
+            avaliable: isHourPast,
          })*/
+
+
+    })
+
+
+    opening.forEach(({hour, avaliable})=>{
+        const li = document.createElement("li")
+
+        li.classList.add("hour")
+        li.classList.add(avaliable ? "hour-avaliable" : "hour-unavailable")
+        li.textContent = hour
+        hours.append(li)
+
 
 
     })
