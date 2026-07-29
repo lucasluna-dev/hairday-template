078933b3256181881764775bddcf69ba22f6cc10
Lucas Luna <lunadev@DESKTOP-DER56QI.localdomain>
2026-07-29 11:35:19 -0300

    Criada a function que filtra os agendamentos do dia

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,16 @@
 import {hoursLoad} from "../form/hours-load.js"
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 
 //Selectiona o input de data.
 const selectedDate = document.getElementById("date")
 
-export function scheduleDay(){
+export async function scheduleDay(){
     //Obtém a data do input
     const date = selectedDate.value
+
+    // Busca na api os agendamentos
+    const dailySchedules = await scheduleFetchByDay({date})
+    console.log(dailySchedules)
 
     // Renderiza as horas disponiveis.
     hoursLoad({date})
