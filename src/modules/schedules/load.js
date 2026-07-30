9128e37c01179bc16d716183f9348a01e0889f7f
Lucas Luna <lunadev@DESKTOP-DER56QI.localdomain>
2026-07-30 12:16:20 -0300

    Renderizando horarios agendados

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,5 +1,6 @@
 import {hoursLoad} from "../form/hours-load.js"
 import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
+import { schedulesShow } from "./show.js"
 
 //Selectiona o input de data.
 const selectedDate = document.getElementById("date")
@@ -10,7 +11,9 @@ export async function scheduleDay(){
 
     // Busca na api os agendamentos
     const dailySchedules = await scheduleFetchByDay({date})
-    console.log(dailySchedules)
+    
+    //exibe os agendamento
+    schedulesShow(dailySchedules)
 
     // Renderiza as horas disponiveis.
     hoursLoad({date})
